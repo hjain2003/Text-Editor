93ce266f8e3b2d03ad7b9f5db014be062cca6e5a
Harsh <harshjainn2003@gmail.com>
2022-10-17 21:47:45 +0530

    added dmode

--- texteditor/src/components/Navbar.js
+++ texteditor/src/components/Navbar.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Navbar(props) {
+
   return (
     <>
     <div id="navbar">
--- texteditor/src/components/Textform.js
+++ texteditor/src/components/Textform.js
@@ -38,18 +38,73 @@ function Textform() {
     settext(localStorage.getItem("Text"));
   }
 
+  const clear=()=>{
+    settext("");
+  }
+
+  const togglemode=()=>{
+    if (mystyle.color=='black')
+    {
+      setstyle({
+        color:'white',
+        backgroundColor:'black',
+      })
+    }
+    else
+    {
+      setstyle({
+        color:'black',
+        backgroundColor:'white',
+      })
+    }
+
+
+    if (mystylo.backgroundColor=='#037dff')
+    {
+      setstylo({
+        color:'white',
+        backgroundColor:'black',
+      })
+    }
+    else
+    {
+      setstylo({
+        color:'white',
+        backgroundColor:'#037dff',
+      })
+    }
+  }
+
+  const [mystyle,setstyle]=useState({
+      color:'black',
+      backgroundColor:'white',
+  })
+  
+  const[mystylo,setstylo]=useState({
+    color:'white',
+    backgroundColor:'#037dff',
+  })
+
   return (
     <>
+    <div id="dark">
+    <label class="switch">
+  <input type="checkbox" onClick={togglemode}/>
+  <span class="slider round"></span>
+</label>
+
+    </div>
     <div id="bigdiv">
     <div id="textbox">
     <div id="wordcount"><i>Word Count : {wordcount}</i></div>
-          <textarea id="area" cols="30" rows="10" value={text} placeholder="Enter text here" onChange={change}></textarea><br/>
-          <button id="uppercase" className='btn' title='Convert to uppercase' onClick={upper}><b><img src="https://img.icons8.com/ios-filled/30/FFFFFF/t.png"/></b></button>
-          <button id="lowercase" className='btn' title='Convert to loweracase' onClick={lower}><b><img src="https://img.icons8.com/ios-glyphs/30/FFFFFF/lowercase.png"/></b></button>
-          <button id="capitalize" className='btn' title='Capitalize each letter' onClick={capitalize}><b>Capitalize</b></button>
-          <button id="speech" className='btn' title="text-to-speech" onClick={speak}><b>Speech</b></button>
-          <button id="save" className='btn' title="Save" onClick={savedisplay}><b>Save</b></button>
-          <button id="display" className='btn' title="Display" onClick={display}><b>Display</b></button>
+          <textarea id="area" cols="30" rows="10" value={text} placeholder="Enter text here" onChange={change} style={mystyle}></textarea><br/>
+          <button id="uppercase" className='btn' title='Convert to uppercase' onClick={upper} style={mystylo}><b><img src="https://img.icons8.com/ios-filled/30/FFFFFF/t.png"/></b></button>
+          <button id="lowercase" className='btn' title='Convert to loweracase' onClick={lower} style={mystylo}><b><img src="https://img.icons8.com/ios-glyphs/30/FFFFFF/lowercase.png"/></b></button>
+          <button id="capitalize" className='btn' title='Capitalize each letter' onClick={capitalize} style={mystylo}><b>Capitalize</b></button>
+          <button id="speech" className='btn' title="text-to-speech" onClick={speak} style={mystylo}><b>Speech</b></button>
+          <button id="save" className='btn' title="Save" onClick={savedisplay} style={mystylo}><b>Save</b></button>
+          <button id="display" className='btn' title="Display" onClick={display} style={mystylo}><b>Display</b></button>
+          <button id="clc" className='btn' title="Clear All" onClick={clear} style={mystylo}><b>Clc</b></button>
       </div>
       </div>
       </>
